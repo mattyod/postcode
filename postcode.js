202bfe13c2222ddb32fa5a92e3ad9fd2ab8407a1
mattyod <mattyod@gmail.com>
2013-07-15 17:03:37 +0100

    added pattern format for HTML5 forms that do not accept modifiers

--- postcode.js
+++ postcode.js
@@ -1,9 +1,11 @@
 var strings = {
     postcode: '(^(?![QVX])[A-Z])([0-9]|[0-9][0-9]|((?![IJZ])[A-Z][0-9][0-9]?)|([0-9][A-HJKSTUW])|((?![IJZ])[A-Z][1-9][ABEHMNPRVWXY]))( ?)[0-9][ABD-HJLNP-UW-Z]{2}$',
+    postcodePattern: '/(^(?![QVXqvx])[A-Za-z])([0-9]|[0-9][0-9]|((?![IJZijz])[A-Za-z][0-9][0-9]?)|([0-9][A-HJKSTUWa-hjkstuw])|((?![IJZijz])[A-Za-z][1-9][ABEHMNPRVWXYabehmnprvwxy]))( ?)[0-9][ABD-HJLNP-UW-Zabd-hjlnp-uw-z]{2}$/'
     santander: '(GIR( ?)0AA)'
 };
 
 module.exports = {
     postcode: new RegExp(strings.postcode, 'i'),
-    postcodeWithSantander: new RegExp(strings.santander + '|' + strings.postcode, 'i')
+    postcodeWithSantander: new RegExp(strings.santander + '|' + strings.postcode, 'i'),
+    postcodePattern: postcodePattern
 };
